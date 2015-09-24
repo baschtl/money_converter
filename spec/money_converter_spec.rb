9f5d31ef594336780f075d0b0b6a61754a027e5b
Sebastian Oelke <dev@soelke.de>
2015-09-24 06:22:33 +0200

    Use strings instead of symbols to match the given interface and updated EUR -> USD rate.

--- spec/money_converter_spec.rb
+++ spec/money_converter_spec.rb
@@ -13,15 +13,15 @@ describe MoneyConverter do
     context "configured with currencies" do
       let(:conversion_rates) do
         {
-          EUR: {
-            DKK: 7.46076,
-            USD: 1.11,
-            BITCOIN: 0.0047
+          'EUR' => {
+            'DKK' => 7.46076,
+            'USD' => 1.11885,
+            'BITCOIN' => 0.0047
           },
-          DKK: {
-            EUR: 0.134033,
-            USD: 0.149708,
-            BITCOIN: 0.000649578
+          'DKK' => {
+            'EUR' => 0.134033,
+            'USD' => 0.149708,
+            'BITCOIN' => 0.000649578
           }
         }
       end
